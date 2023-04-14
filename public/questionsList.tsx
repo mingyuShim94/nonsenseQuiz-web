5ee643eaa1c2999b4096ed817b1cd66f794070f8
mingyuShim94 <asdgfhgjk90@gmail.com>
2023-04-14 23:53:46 +0900

    first commit

---
+++ public/questionsList.tsx
@@ -0,0 +1,107 @@
+export default [
+  {
+    answer: "불매운동",
+    imageUrl: "/images/1-min.png",
+    hint: "ㅂㅁㅇㄷ",
+  },
+  {
+    answer: "다이소",
+    imageUrl: "/images/2-min.png",
+    hint: "ㄷㅇㅅ",
+  },
+  {
+    answer: "다이아몬드",
+    imageUrl: "/images/3-min.png",
+    hint: "ㄷㅇㅇㅁㄷ",
+  },
+  {
+    answer: "검문소",
+    imageUrl: "/images/4-min.png",
+    hint: "ㄱㅁㅅ",
+  },
+  {
+    answer: "캡사이신",
+    imageUrl: "/images/5-min.png",
+    hint: "ㅋㅅㅇㅅ",
+  },
+  {
+    answer: "개런티",
+    imageUrl: "/images/6-min.png",
+    hint: "ㄱㄹㅌ",
+  },
+  {
+    answer: "알송달송",
+    imageUrl: "/images/7-min.png",
+    hint: "ㅇㅅㄷㅅ",
+  },
+  {
+    answer: "박스오피스",
+    imageUrl: "/images/8-min.png",
+    hint: "ㅂㅅㅇㅍㅅ",
+  },
+  {
+    answer: "무에타이",
+    imageUrl: "/images/9-min.png",
+    hint: "ㅁㅇㅌㅇ",
+  },
+  {
+    answer: "바이오리듬",
+    imageUrl: "/images/10-min.png",
+    hint: "ㅂㅇㅇㄹㄷ",
+  },
+  {
+    answer: "세계종말",
+    imageUrl: "/images/11-min.png",
+    hint: "ㅅㄱㅈㅁ",
+  },
+  {
+    answer: "우거지",
+    imageUrl: "/images/12-min.png",
+    hint: "ㅇㄱㅈ",
+  },
+  {
+    answer: "두부한모",
+    imageUrl: "/images/13-min.png",
+    hint: "ㄷㅂㅎㅁ",
+  },
+  {
+    answer: "월드컵",
+    imageUrl: "/images/14-min.png",
+    hint: "ㅇㄷㅋ",
+  },
+  {
+    answer: "무지개",
+    imageUrl: "/images/15-min.png",
+    hint: "ㅁㅈㄱ",
+  },
+  {
+    answer: "자녀",
+    imageUrl: "/images/16-min.png",
+    hint: "ㅈㄴ",
+  },
+  {
+    answer: "인사돌",
+    imageUrl: "/images/17-min.png",
+    hint: "ㅇㅅㄷ",
+  },
+  {
+    answer: "언덕",
+    imageUrl: "/images/18-min.png",
+    hint: "ㅂㅁㅇㄷ",
+  },
+  {
+    answer: "인쇄소",
+    imageUrl: "/images/19-min.png",
+    hint: "ㅇㅅㅅ",
+  },
+  {
+    answer: "중고차",
+    imageUrl: "/images/20-min.png",
+    hint: "ㅈㄱㅊ",
+  },
+  {
+    answer: "뱀파이어",
+    imageUrl: "/images/21-min.png",
+    hint: "ㅂㅍㅇㅇ",
+  },
+];
