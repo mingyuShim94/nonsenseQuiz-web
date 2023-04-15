be183c6c9ed68a92bd789ee2d7844323a48a9e63
mingyuShim94 <asdgfhgjk90@gmail.com>
2023-04-15 15:11:13 +0900

    img mod

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center h-screen ">
         {!gameStarted && (
           <>
-            <h1 className="text-5xl font-bold mb-8">
+            <h1 className="text-5xl font-bold mb-8 md:text-lg">
               Ai로 만든 넌센스그림퀴즈
             </h1>
             <button
